e2fa371d483b260b3c916dc7eb2421b1a835baaf
zyuzyu.sako <sakosakozaq12wsx@gmail.com>
2020-09-04 10:26:17 +0000

    heroku

--- database/migrations/2020_09_04_110219_add_status_to_tasks_table.php
+++ database/migrations/2020_09_04_110219_add_status_to_tasks_table.php
@@ -14,8 +14,7 @@ class AddStatusToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('status',10);
-            $table->string('status',10);
+            $table->string('status', 10);
         });
     }
 
@@ -27,7 +26,7 @@ class AddStatusToTasksTable extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->dropClumn('status');
+            $table->dropColumn('status');
         });
     }
 }
