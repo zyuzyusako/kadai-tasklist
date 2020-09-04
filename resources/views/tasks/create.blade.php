610472cae42ecb347f107832dc259e75bff116bb
zyuzyu.sako <sakosakozaq12wsx@gmail.com>
2020-09-04 02:47:41 +0000

    error-task

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
 
+ @if (count($errors) > 0)
+        <ul class="alert alert-danger" role="alert">
+            @foreach ($errors->all() as $error)
+                <li class="ml-4">{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+
     <h1>作成ページ</h1>
 
     <div class="row">
