0901490798a375914dc85f7e633befb8924ac4b8
zyuzyu.sako <sakosakozaq12wsx@gmail.com>
2020-09-07 08:03:33 +0000

    top page

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@
 Route::get('/','TasksController@index');
 Route::resource('tasks','TasksController');
 
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 /*
 Route::get('/', function () {
     return view('welcome');
