d3974654df5bd5a88d60f8a817d8f33897d31f8b
zyuzyu.sako <sakosakozaq12wsx@gmail.com>
2020-09-07 08:18:58 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     <div class="center jumbotron">
         <div class="text-center">
-            <h1>Welcome to the Taskilists</h1>
+            <h1>Welcome to the Tasklists</h1>
+            {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
