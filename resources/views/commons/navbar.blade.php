117280b20badd4c9a174c2571223ea69d3d3abee
zyuzyu.sako <sakosakozaq12wsx@gmail.com>
2020-09-11 05:26:33 +0000

    user pages2

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -16,12 +16,12 @@
                     <li class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
                         <ul class="dropdown-menu dropdown-menu-right">
-                            {{-- ユーザ詳細ページへのリンク --}}
-                            <li class="dropdown-item"><a href="#">My profile</a></li>
-                            <li class="dropdown-divider"></li>
-                            {{-- ログアウトへのリンク --}}
-                            <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
-                        </ul>
+                                {{-- ユーザ詳細ページへのリンク --}}
+                                <li class="dropdown-item">{!! link_to_route('users.show', 'My profile', ['user' => Auth::id()]) !!}</li>
+                                <li class="dropdown-divider"></li>
+                                {{-- ログアウトへのリンク --}}
+                                <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
+                            </ul>
                     </li>
                 @else
                     {{-- ユーザ登録ページへのリンク --}}
